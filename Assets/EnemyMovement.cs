a961250648a60e21af017a4870a716a13a7844b8
frank184 <francoisbelanger1993@hotmail.com>
2018-07-04 17:26:11 -0400

    init

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public float speed = 10f;
+    private int wavepointIndex = 0;
+    private Transform target;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+
+    private void Update()
+    {
+        Vector3 waypointDirection = target.position - transform.position;
+        transform.Translate(waypointDirection.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(target.position, transform.position) <= 0.4f)
+        {
+            if (wavepointIndex < Waypoints.waypoints.Length)
+                target = Waypoints.waypoints[wavepointIndex++];
+            else Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    public static Transform[] waypoints;
+
+
+    private void Awake()
+    {
+        waypoints = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            waypoints[i] = transform.GetChild(i);
+    }
+}
